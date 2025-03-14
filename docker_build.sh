4b8567dde1920fa356149fd82ef383537126c633
fm-labs <flowmotionlabs@gmail.com>
2025-03-14 23:25:10 +0100

    Updated publish workflow

--- docker_build.sh
+++ docker_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#echo "Pull latest base image..."
-#docker pull python:3.13-slim
+echo "Pull latest base image..."
+docker pull python:3.13-alpine
 
 echo "Building multi-platform image..."
 docker buildx build -t kmc:latest \
