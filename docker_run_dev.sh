92fce2a1a136ed74f6afa5e4e9d6c65076c05ad5
fm-labs <flowmotionlabs@gmail.com>
2025-03-22 10:47:26 +0100

    Updated submodules to version 0.8.1.2

--- docker_run_dev.sh
+++ docker_run_dev.sh
@@ -6,6 +6,7 @@ docker run -it --rm \
   --name kmc-dev \
   -p 3080:3080 \
   -p 3443:3443 \
+  -p 5000:5000 \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v /var/lib/docker/volumes:/var/lib/docker/volumes:ro \
   -v kmc_dev_data:/app/data \
