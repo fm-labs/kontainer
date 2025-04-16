150d93b843d611ad3df3a151f9f652af923ae186
fm-labs <flowmotionlabs@gmail.com>
2025-04-16 05:05:51 +0200

    Updated helper scripts

---
+++ build-image.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Pull latest base image..."
+docker pull python:3.13-alpine
+
+echo "Building multi-platform image..."
+docker buildx build -t kontainer:latest \
+  --progress=plain \
+  --platform linux/amd64,linux/arm64 \
+  -f ./Dockerfile-alpine \
+  .
+
+
+if [ $1 = "--scan" ]; then
+  echo "Scanning image for vulnerabilities"
+  rm -rf build/
+  mkdir -p build/
+  docker scout cves kontainer:latest > build/scout-cves.txt
+  docker scout recommendations kontainer:latest > build/scout-recommendations.txt
+  exit 0
+fi
